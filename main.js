a8e4d87ec8ce54aaebda03aa29f792c3f887e321
majogn <majogn99@gmail.com>
2023-02-13 22:10:13 -0300

    avance preentrega2

--- main.js
+++ main.js
@@ -1,87 +1,80 @@
-function saludoDireccion () {
-let ingresoNombre=prompt("Ingresa tu nombre")
-let ingresoDireccion=prompt("Ingresa la direccion en la que será entregado el pedido (si es edificio, recuerda ingresar el apto)")
+const productos =[
+    {nombre: "Tequeños", precio: 100, stock: 200, vendido:false},
+    {nombre: "Mandocas", precio: 150, stock: 180, vendido:false},
+    {nombre: "Cachapas", precio: 800, stock: 100, vendido:false},
+    {nombre: "Empanadas", precio: 200, stock: 200, vendido:false}
+]
 
-if ((ingresoNombre != "") && (ingresoDireccion != "")){
-    alert("hola "+ ingresoNombre + "!" + "Tu pedido será enviado a: " + ingresoDireccion)
-}else{
-    alert("ERROR: complete los campos indicados")
-}
+const cliente=[]
+const compra= []
+
+
+function datosInicio(){
+    let ingresoNombre=prompt("Ingresa tu nombre")
+    let ingresoDireccion=prompt("Ingresa la direccion en la que será entregado el pedido (si es edificio, recuerda ingresar el piso")
+    if((ingresoNombre != "") && (ingresoDireccion != "")){
+        cliente.push(ingresoNombre)
+        cliente.push(ingresoDireccion)
+        alert("Bienvenido a Mas que Sabor!, su nombre y su direccion fueron registrados respectivamente como: " + cliente.join("\n"))
+        }
+        else{
+            alert("ERROR: recargue la pagina e ingrese los datos solicitados")
+        }
 }
 
-function multiplicar(valor1, valor2){
-    let resultado=valor1*valor2;
-    return resultado
-}
 
-const tequeños= 100
-const mandocas= 150
-const cachapas= 800
-const empanadas= 200
-let precioFinal = 0
+function menu(){
+    alert("A continuacion los productos que conforman nuestro menú")
+    let menuProductos= productos.map(productos => productos.nombre + " " + productos.precio +"$")
+    alert(menuProductos.join(" - "))
+}
 
 let bienvenida=alert("Bienvenido/a a Mas que Sabor")
-saludoDireccion ()
-let seleccion = prompt("Elige de nuestros ricos productos disponibles con sabor venezolano: \n 1 Tequeños \n 2 Mandocas \n 3 Cachapas \n 4 Empanadas \nEscribe 0 para finalizar")
+datosInicio()
+if(cliente.length>= 1){
+menu()
+
+let seleccion = prompt("Elige de nuestros ricos productos disponibles con sabor venezolano: \n 1 Tequeños \n 2 Mandocas \n 3 Cachapas \n 4 Empanadas \nEscribe 0 para cancelar ") 
 
 
 while (seleccion !="0"){
-    let precioProd
+    let precioProd=0
     switch (seleccion) {
         case "1":
-            let cantidadTequeños = parseInt( prompt("ingrese cuantas unidades"))
-            if ((cantidadTequeños > 0) && (cantidadTequeños != "")){
-                precioProd = multiplicar(cantidadTequeños,tequeños)
-                precioFinal = parseInt(precioFinal + precioProd)
-            alert("se agregó " + cantidadTequeños + " tequeños y el precio final es $ " + precioFinal)
-            }
-            else{
-                alert("campo invalido, agregue la cantidad o escriba 0 para finalizar")
-            }
+            precioProd=productos[0].precio
             break;
             
         case "2":
-            let cantidadMandocas = parseInt( prompt("ingrese cuantas unidades"))
-            if ((cantidadMandocas > 0) && (cantidadMandocas != "")){
-                precioProd = multiplicar(cantidadMandocas,mandocas)
-                precioFinal = parseInt(precioFinal + precioProd)
-            alert("se agregó " + cantidadMandocas + " mandocas y el precio final es $ " + precioFinal)
-                }
-                else{
-                    alert("campo invalido, agregue la cantidad o escriba 0 para finalizar")
-                }
-                break;
-                
-
+            precioProd=productos[1].precio
+            break;
+        
         case "3":
-            let cantidadCachapas = parseInt( prompt("Las cachapas son rellena con queso, ingrese cuantas unidades"))
-            if ((cantidadCachapas > 0) && (cantidadCachapas != "")){
-                precioProd = multiplicar(cantidadCachapas,cachapas)
-                precioFinal = parseInt(precioFinal + precioProd)
-            alert("se agregó " + cantidadCachapas + " cachapas y el precio final es $ " + precioFinal)
-                }
-                else{
-                    alert("campo invalido, agregue la cantidad o escriba 0 para finalizar")
-                }
-                break;
-            
+            precioProd=productos[2].precio
+            break;
+        
         case "4":
-            let cantidadEmpanadas = parseInt( prompt("Las empanadas son rellenas de carne mechada, ingrese cuantas unidades"))
-            if ((cantidadEmpanadas > 0) && (cantidadEmpanadas != "")){
-                precioProd = multiplicar(cantidadEmpanadas,empanadas)
-                precioFinal = parseInt(precioFinal + precioProd)
-            alert("se agregó " + cantidadEmpanadas + " empanadas y el precio final es $ " + precioFinal)
-                }
-                else{
-                    alert("campo invalido, agregue la cantidad o escriba 0 para finalizar")
-                }
-                break;
-            
+            precioProd=productos[3].precio
+            break;
+        
         default:
-            alert("Orden vacia")
+            alert("Opcion inválida")
             break;
     }
-seleccion = prompt("Elige de nuestros ricos productos disponibles con sabor venezolano: \n 1 Tequeños \n 2 Mandocas \n 3 Cachapas \n 4 Empanadas \n Escribe 0 para finalizar");
+    let unidades=parseInt(prompt("Cuantas unidades llevas?"))
+    compra.push({productos, unidades, precioProd})
+    seleccion = prompt("Elige de nuestros ricos productos disponibles con sabor venezolano: \n 1 Tequeños \n 2 Mandocas \n 3 Cachapas \n 4 Empanadas \nEscribe 0 para finalizar \n 9 para ver su compra")
+
+    if(seleccion=="9"){
+        compra.forEach((compraFinal) =>{
+             alert(`producto: ${compraFinal.productos.nombre}, unidades: ${compraFinal.unidades} y el total pagar por cada producto seleccionado seria ${compraFinal.unidades * compraFinal.precioProd}`)
+        })
+            alert("Gracias por preferirnos!")
+            break;
+    }
+    
 }
 
-alert("Gracias por preferirnos!")
+if(seleccion=="0"){
+    ("Su compra fue cancelada")
+}
+}
